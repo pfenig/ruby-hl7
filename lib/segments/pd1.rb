a92941240cad1120857b12432cac6468a3dc1d35
whtt-eric <eric@wehaventthetime.com>
2011-02-09 15:13:27 -0500

    added PD1 segment definition

---
+++ lib/segments/pd1.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+require 'ruby-hl7'
+class HL7::Message::Segment::PD1 < HL7::Message::Segment
+  weight 4
+  add_field :living_dependency
+  add_field :living_arrangement
+  add_field :patient_primary_facility
+  add_field :patient_primary_care_provider
+  add_field :student_indicator
+  add_field :handicap
+  add_field :living_will
+  add_field :organ_donor
+  add_field :separate_bill
+  add_field :duplicate_patient
+  add_field :publicity_indicator
+  add_field :protection_indicator
+end
